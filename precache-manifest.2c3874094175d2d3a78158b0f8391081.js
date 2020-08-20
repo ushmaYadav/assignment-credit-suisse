a3bb632776a367b95f54eafbf904c7f22ee76dd1
Ushma Yadav <ushma.yadav@nearbuy.com>
2020-08-21 00:50:40 +0530

    Updates

---
+++ precache-manifest.2c3874094175d2d3a78158b0f8391081.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01d086f7c6da1f34316594a2a152890f",
+    "url": "/github.io/assignment-credit-suisse/index.html"
+  },
+  {
+    "revision": "e3799ff4e8968cf4f833",
+    "url": "/github.io/assignment-credit-suisse/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3581eeb10a855ad31e98",
+    "url": "/github.io/assignment-credit-suisse/static/js/2.ce159d2d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/assignment-credit-suisse/static/js/2.ce159d2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3799ff4e8968cf4f833",
+    "url": "/github.io/assignment-credit-suisse/static/js/main.d35e91c6.chunk.js"
+  },
+  {
+    "revision": "ab384717e65337866ef6",
+    "url": "/github.io/assignment-credit-suisse/static/js/runtime-main.1f5c004f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github.io/assignment-credit-suisse/static/media/logo.5d5d9eef.svg"
+  }
+]);
